94379835f02e95173bd418890d934c28dec701c2
Roman <Roman.Vengl1@gmail.com>
2020-01-20 16:05:20 +0200

    Updates

---
+++ precache-manifest.9a308a8444c6fb6adc41282be15d9b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eff419850f8765875f0cc22b77184db",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "ebb15c8b1a8f28ae2524",
+    "url": "/goit-react-hw-01-components/static/css/main.edd71f0a.chunk.css"
+  },
+  {
+    "revision": "1224a5e0e7d8ff04f4dc",
+    "url": "/goit-react-hw-01-components/static/js/2.25b66531.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.25b66531.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebb15c8b1a8f28ae2524",
+    "url": "/goit-react-hw-01-components/static/js/main.5f4a9493.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
